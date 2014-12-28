004bb12dc4a818c75581c2411970820ede2c4256
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-12-28 21:26:51 +0100

    Added a function for one off timers

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,8 @@ use React\EventLoop\LoopInterface;
 use React\Promise\Deferred;
 
 /**
- * @param LoopInterface $loop
+ * @param LoopInterface $loop ReactPHP event loop.
+ *
  * @return \React\Promise\Promise
  */
 function futurePromise(LoopInterface $loop)
@@ -19,13 +20,29 @@ function futurePromise(LoopInterface $loop)
 }
 
 /**
- * @param LoopInterface $loop
+ * @param LoopInterface $loop ReactPHP event loop.
+ *
  * @return \React\Promise\Promise
  */
 function nextPromise(LoopInterface $loop)
 {
     $deferred = new Deferred();
     $loop->nextTick(function () use ($deferred) {
+        $deferred->resolve();
+    });
+    return $deferred->promise();
+}
+
+/**
+ * @param LoopInterface $loop     ReactPHP event loop.
+ * @param integer       $interval The number of seconds to wait before execution.
+ *
+ * @return \React\Promise\Promise
+ */
+function timedPromise(LoopInterface $loop, $interval)
+{
+    $deferred = new Deferred();
+    $loop->addTimer($interval, function () use ($deferred) {
         $deferred->resolve();
     });
     return $deferred->promise();
--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -53,4 +53,29 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase
         });
         $this->assertTrue($callbackCalled);
     }
+
+    public function testTimedPromise()
+    {
+        $loop = $this->getMock('React\EventLoop\StreamSelectLoop', [
+            'addTimer',
+        ]);
+
+        $loop
+            ->expects($this->once())
+            ->method('addTimer')
+            ->with($this->isType('numeric'), $this->isType('callable'))
+            ->will($this->returnCallback(function ($interval, $resolveCb) {
+                $resolveCb('foo.bar' . (string)microtime(true));
+            }))
+        ;
+
+        $promise = \WyriHaximus\React\timedPromise($loop, 123);
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $promise);
+
+        $callbackCalled = false;
+        $promise->then(function () use (&$callbackCalled) {
+            $callbackCalled = true;
+        });
+        $this->assertTrue($callbackCalled);
+    }
 }
