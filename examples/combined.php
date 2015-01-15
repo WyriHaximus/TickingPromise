e9f885618d052421828997ea75b78e72156f2bef
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-01-15 08:15:50 +0100

    Added tickingPromise to combined example

--- examples/combined.php
+++ examples/combined.php
@@ -4,17 +4,24 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 $loop = \React\EventLoop\Factory::create();
 
-\WyriHaximus\React\futurePromise($loop)->then(function () {
-    return time();
-})->then(function ($time) use ($loop) {
+\React\Promise\all([
+    \WyriHaximus\React\futurePromise($loop)->then(function () {
+        return time();
+    }),
+    \WyriHaximus\React\tickingPromise($loop, 0.001, function () {
+        echo '.';
+        return mt_rand(0, 1000) == 13;
+    }),
+])->then(function ($time) use ($loop) {
     return \WyriHaximus\React\nextPromise($loop)->then(function () use ($time) {
-        return $time;
+        return $time[0];
     });
 })->then(function ($time) use ($loop) {
     return \WyriHaximus\React\timedPromise($loop, 3)->then(function () use ($time) {
         return $time;
     });
 })->then(function ($time) {
+    echo PHP_EOL;
     echo DateTime::createFromFormat('U', $time)->format('r'), PHP_EOL;
     echo DateTime::createFromFormat('U', time())->format('r'), PHP_EOL;
     echo 'Done', PHP_EOL;
