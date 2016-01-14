57cd76118444f7ff7f5b4d25f4cdb73ded049bd1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-01-14 08:19:04 +0100

    Ignore code coverage for the functions include file

--- src/functions_include.php
+++ src/functions_include.php
@@ -2,6 +2,8 @@
 
 namespace WyriHaximus\React;
 
+// @codeCoverageIgnoreStart
 if (!function_exists('WyriHaximus\React\futurePromise')) {
     require __DIR__ . '/functions.php';
 }
+// @codeCoverageIgnoreEnd
